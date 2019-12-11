17af98df26c6a2a54063f6845da47366c9fea0ce
Mary Álvarez <maryalvarez@Marys-MacBook-Pro.local>
2019-12-11 06:09:11 -0600

    components structure

--- app.js
+++ app.js
@@ -1,7 +1,67 @@
+Vue.component('CoinDetail', {
+    props: ['coin'],
+    data() {
+        return {
+            showPrices: false,
+            value: 0
+        }
+    },
+    methods: {
+        toggleShowPrices() {
+            this.showPrices = !this.showPrices
+        }
+    },
+
+    computed: {
+        convertedValue(){
+            if (!this.value) {
+                return 0
+            }
+   
+            return this.value / this.coin.price
+           }
+    },
+    template: `
+    <div>
+        <img 
+            v-on:mouseover="toggleShowPrices"
+            v-on:mouseout="toggleShowPrices" 
+            v-bind:src="coin.img" 
+            v-bind:alt="coin.name"
+        >
+
+        <h1 v-bind:class="coin.changePercent>0 ? 'green' : 'red' "> 
+            {{ title }}
+            <span v-if="coin.changePercent>0">c:</span>
+            <span v-else-if="coin.changePercent<0">:c</span>
+            <span v-else>suerte</span>
+        </h1>
+        <span v-on:click="toggleShowPrices">
+            {{ showPrices ? 'Ocultar precios' : 'Ver precios'}}
+        </span>
+
+        <input type="number" v-model="value">
+        <span> {{ convertedValue }}</span>
+
+        <ul v-show="showPrices"> 
+            <li 
+                v-bind:class="{ orange: p.value === coin.price, red: p.value < coin.price, green: p.value > coin.price }"
+                v-for="(p, i) in coin.pricesWithDays" 
+                v-bind:key="p.day"> 
+                {{ p.day }} - {{ p.value }}
+            </li>
+        /ul>
+    </div>
+   
+    `
+})
 new Vue({
     el: '#app',
     data() {
         return {
+            btc: {
+                
+            }
             name: 'Bitcoin',
             symbol: 'BTC',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
@@ -25,16 +85,7 @@ new Vue({
     computed: {
         title() {
             return `${this.name} - ${this.symbol}`
-        },
-
-        convertedValue(){
-         if (!this.value) {
-             return 0
-         }
-
-         return this.value / this.price
         }
-
     },
 
     watch: {
